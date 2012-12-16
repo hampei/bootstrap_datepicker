a41cf01d3118fbfd4a53dcdc5b37a81a27880f42
Derek Stavis <dekestavis@gmail.com>
2012-12-16 16:50:15 -0200

    inobstrusive javascript bet

--- vendor/assets/javascripts/bootstrap-datepicker.js
+++ vendor/assets/javascripts/bootstrap-datepicker.js
@@ -16,6 +16,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * ========================================================= */
+
+ $(function() {
+ 	$('.datetime_select').datepicker({})
+ })
  
 !function( $ ) {
 	
